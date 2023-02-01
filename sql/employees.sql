58b48751034f28264605ac1b519f51db35fddb64
Binh Ngo <bnngo97@gmail.com>
2023-02-01 13:17:51 -0800

    created roles, departments, and employees in employee.sql'

--- sql/employees.sql
+++ sql/employees.sql
@@ -3,15 +3,15 @@ CREATE DATABASE employees;
 USE employees;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL UNSIGNED NOT NULL,
-    department_id INT UNSIGNED NOT NULL,
+    department_id INT UNSIGNED NOT NULL
 )
 
 CREATE TABLE employee (
@@ -20,4 +20,34 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT UNSIGNED NOT NULL,
     manager_id INT UNSIGNED
-)
+);
+
+USE employees;
+INSERT INTO department (name)
+VALUES ('Corporate'), ('Back of House'), ('Front of House'), ('Marketing');
+
+INSERT INTO role (title, salary, department_id)
+VALUES 
+    ('Chief Executive Officer', 150000, 1),
+    ('Chief Operations Officer', 120000, 1),
+    ('Chef de Cuisine', 80000, 2),
+    ('Sous Chef', 60000, 2),
+    ('Line Cook', 35000, 2),
+    ('General Manager', 90000, 3),
+    ('Server', 70000, 3),
+    ('Senior Marketer', 60000, 4),
+    ('Social Media Content Creator', 6000, 4);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Marco', 'White', 1, NULL),
+    ('Gordon', 'Ramsay', 2, 1),
+    ('Heston', 'Blumenthal', 3, NULL),
+    ('Bjorn', 'Frantzen', 4, 3),
+    ('Tristan', 'Farmer', 5, 3),
+    ('Anthony', 'Bourdain', 6, NULL),
+    ('Kyle', 'Connaughton', 7, 6),
+    ('Andrew', 'Zimmern', 8, NULL),
+    ('Paula', 'Deen', 9, 8);
+
