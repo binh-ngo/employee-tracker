38b77c5ed29055824dbc445322d4b81fa75ff5d5
Binh Ngo <bnngo97@gmail.com>
2023-02-02 09:54:23 -0800

    created view employee by manager and dept functions, and created the add employee function

--- index.js
+++ index.js
@@ -112,30 +112,70 @@ function allDepts() {
     });
 }
 function allRoles() {
+    const query = `SELECT role.id, role.title, department.name AS department, role.salary 
+    FROM role 
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY role.id;
+    `
 
+    connection.query(query, (err, data) => {
+        if(err) throw err;
+        console.log('\n ------------------------------');
+        console.log(`        VIEW ALL ROLES`);
+        console.log('\n ------------------------------');
+        console.table(data);
+        prompt();
+    });
 }
 function allEmployees() {
     const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
     FROM employee
     LEFT JOIN employee manager on manager.id = employee.manager_id
     INNER JOIN role ON (role.id = employee.role_id)
-    INNER JOIN department oN (department.id = role.department_id)
+    INNER JOIN department ON (department.id = role.department_id)
     ORDER BY employee.id;`
 
     connection.query(query, (err, data) => {
         if(err) throw err;
         console.log('\n ------------------------------');
-        console.log('VIEW ALL EMPLOYEES');
+        console.log(`     VIEW ALL EMPLOYEES`);
         console.log('\n ------------------------------');
         console.table(data);
         prompt();
     });
 }
 function emByDept() {
+    const query = ` SELECT department.name AS department, role.title, employee.id, employee.first_name, employee.last_name
+    FROM employee
+    LEFT JOIN role ON (role.id = employee.role_id)
+    LEFT JOIN department ON (department.id = role.department_id)
+    ORDER BY department.name;`
 
+    connection.query(query, (err, data) => {
+        if(err) throw err;
+        console.log('\n ------------------------------');
+        console.log(`     VIEW ALL EMPLOYEES BY DEPARTMENT`);
+        console.log('\n ------------------------------');
+        console.table(data);
+        prompt();
+    });
 }
 function emByManager() {
+    const query = `SELECT CONCAT(manager.first_name, ' ', manager.last_name) AS manager, department.name AS department, employee.id, employee.first_name, employee.last_name, role.title
+    FROM employee
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role ON (role.id = employee.role_id && employee.manager_id != 'NULL')
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY manager;`;
 
+    connection.query(query, (err, data) => {
+        if(err) throw err;
+        console.log('\n ------------------------------');
+        console.log(`     VIEW ALL EMPLOYEES BY MANAGER`);
+        console.log('\n ------------------------------');
+        console.table(data);
+        prompt();
+    });
 }
 function addDept() {
 
@@ -143,8 +183,68 @@ function addDept() {
 function addRole() {
 
 }
-function addEmployee() {
-
+async function addEmployee() {
+    const addName = await inquirer.prompt(getName());
+    connection.query('SELECT role.id, role.title FROM role ORDER BY role.id;', async(err, data) => {
+        if (err) throw err;
+        const {role} = await inquirer.prompt(
+            [
+                {
+                    name: "role",
+                    type: "list",
+                    choices: () => data.map(data => data.title),
+                    message: "What is the employee role? "
+                }
+            ]);
+            let roleId;
+            for (const row of data) {
+                if (row.title === role) {
+                    roleId = row.id;
+                    continue;
+                }
+            }
+            connection.query('SELECT * FROM employee', async (err, data) => {
+                if (err) throw err;
+                let choices = data.map(data => `${data.first_name} ${data.last_name}`);
+                choices.push('none');
+                let { manager } = await inquirer.prompt([
+                    {
+                        name: 'manager',
+                        type: 'list',
+                        choices: choices,
+                        message: 'Choose the employee Manager: '
+                    }
+                ]);
+                let managerID;
+                let managerName;
+                if (manager === 'none') {
+                    managerID = null;
+                } else {
+                    for (const info of data) {
+                        info.fullName = `${info.first_name} ${info.last_name}`;
+                        if (info.fullName === manager) {
+                            managerID = info.id;
+                            managerName = info.fullName;
+                            console.log(managerID);
+                            console.log(managerName);
+                            continue;
+                        }
+                    }
+                }
+                console.log('Employee successfully added.');
+                connection.query('INSERT INTO employee SET ?',
+                {
+                    first_name: addName.first,
+                    last_name: addName.last,
+                    role_id: roleId,
+                    manager_id: parseInt(managerID)
+                },
+                (err, data) => {
+                    if (err) throw err;
+                    prompt();
+                })
+            })
+    })
 }
 function rmvEmployee() {
 
@@ -154,4 +254,20 @@ function updateEmRole() {
 }
 function updateEmManager() {
 
+}
+
+function getName() {
+    return (
+        [
+            {
+                name: "first",
+                type: "input",
+                message: "Enter the first name: "
+            },
+            {
+                name: "last",
+                type: "input",
+                message: "Enter the last name: "
+            }
+        ]);
 }
